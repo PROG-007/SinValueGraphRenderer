0ce99c4bc899603f000d1a56d12f23a7ac5ffcff
Raghav G <83812669+PROG-007@users.noreply.github.com>
2023-03-21 00:19:22 +0530

    fixed the logic a bit

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import os
 import math
 
-degrees = 181
-degreesIndex=0
-degreesVar = 0
-a = [[' ']*degrees for i in range(21)]
+degreeSpanSingleScreen = 90
+verticalPrecisionParameter = 50
+graphYPosIndex = 0
+degreeFreeFlow = 0
+steepness = 10
 
+a = [[' ']*degreeSpanSingleScreen for i in range(verticalPrecisionParameter+1)]
 
 def printGraph():
     for i in range(len(a)):
         for j in range(len(a[i])):
             print(a[i][j], end="")
         print()
-
-
 while True:
-    os.system("cls")
-    sinVal = int(math.sin(degreesIndex/10)*10)
-    if sinVal > 9:
-        sinVal = 9
-    for i in range(21):
-        a[i][degreesVar] = " "
-    a[10-(sinVal)][degreesVar] = "*"
+    sinVal = int(math.sin(math.radians(degreeFreeFlow))*10)
+    if sinVal > 10:
+        sinVal = 10
+    for i in range(verticalPrecisionParameter+1):
+        a[i][graphYPosIndex] = " "
+    a[(verticalPrecisionParameter//2)-(sinVal)][graphYPosIndex] = "."
     printGraph()
-    if degreesVar >= degrees-1:
-        degreesVar = 0
-    degreesVar += 1
-    degreesIndex += 1
+    if graphYPosIndex >= degreeSpanSingleScreen-1:
+        graphYPosIndex = 0
+    degreeFreeFlow += steepness
+    graphYPosIndex += 1
+    os.system("cls")
