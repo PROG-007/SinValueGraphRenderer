87ea80597da82c3664287cd0972c419dd3244e36
Raghav G <83812669+PROG-007@users.noreply.github.com>
2023-03-22 18:00:02 +0530

    v1.0
    All features added and bugs fixed!

--- main.py
+++ main.py
@@ -1,59 +1,50 @@
 import os
 import math
 import time
-os.system("cls")
-switch = False
-degreeSpanSingleScreen = 180
-verticalPrecisionParameter = 50
-graphYPosIndex = 0
-degreeFreeFlow = 0
-steepness = 10
-character = ['O', '#']
-characterSelection = False
 
-while True:
-    os.system("cls")
-    a = str(input(
-        f"Enter the amount of degrees you want to span in 1 screen ({degreeSpanSingleScreen} is default and recommended) : "))
-    if a.isdigit():
-        if int(a) > 0:
-            degreeSpanSingleScreen = int(a)
-            break
+
+switch = False # user defined
+
+degreeSpanSingleScreen = 180 # user defined
+degreeSpanSingleScreenFeasibleLimit = 180 # constant
+steepness = 10 # user defined
+steepnessBreakingPoint = 30 # constant
+
+graphPadding = 50 # constant
+graphYPosIndex = 0 # dynamic
+degreeFreeFlow = 0 # dynamic
+
+character = ['O', '#'] # constant
+characterSelection = True # dynamic
+
+def printGraph():
+    for i in range(len(graphData)):
+        for j in range(len(graphData[i])):
+            print(graphData[i][j], end="")
+        print("")
+
+def customInputHandler(varA, varB, correctionConstant = 1):
+    while True:
+        os.system("cls")
+        a = str(input(f"Enter the amount of steepness you want [1 < value < {varB}(Breaking Point)] [{varA} is default and recommended for best results] : "))
+        if a.isdigit():
+            if int(a) > 1:
+                temp = int(a)//correctionConstant
+                varA = ((temp)-((temp)%2))*correctionConstant
+                break
+            else:
+                print(f"Please enter a numeric value in the range [1 < value < {varB}(Breaking Point)] not anything else!")
+                os.system("PAUSE")
         else:
-            print(f"Please enter a positive value not anything else!")
+            print("Please enter a numeric value not anything else!")
             os.system("PAUSE")
-    else:
-        print("Please enter a numeric value not anything else!")
-        os.system("PAUSE")
-while True:
-    os.system("cls")
-    a = str(input(
-        f"Enter the amount of Vertical Precision you want in the curve ({verticalPrecisionParameter} is default and recommended) : "))
-    if a.isdigit():
-        if int(a) > 0:
-            verticalPrecisionParameter = int(a)
-            break
-        else:
-            print("Please enter a positive value not anything else!")
-            os.system("PAUSE")
-    else:
-        print("Please enter a numeric value not anything else!")
-        os.system("PAUSE")
-while True:
-    os.system("cls")
-    a = str(input(
-        f"Enter the amount of steepness you want ({steepness} is default and recommended) : "))
-    if a.isdigit():
-        if int(a) > 0:
-            steepness = int(a)
-            break
-        else:
-            print("Please enter a positive value not anything else!")
-            os.system("PAUSE")
-    else:
-        print("Please enter a numeric value not anything else!")
-        os.system("PAUSE")
-while True:
+    return varA
+
+os.system("cls")
+degreeSpanSingleScreen = customInputHandler(degreeSpanSingleScreen, degreeSpanSingleScreenFeasibleLimit, 10)
+steepness = customInputHandler(steepness, steepnessBreakingPoint)
+
+while True: # render type switch selection input handler
     os.system("cls")
     b = str(input("Which do you prefer: \n[A] Live updating(moving) wave or all points in wave\n are updated simultaneously\n[B] Point by point update of each point in the wave and a \nlive refresh as the new wave overlaps the old. : "))
     if b.lower() == 'a':
@@ -66,33 +57,24 @@ while True:
         print("Please enter A or B as an option not anything else!")
         os.system("PAUSE")
 
-a = [[' ']*degreeSpanSingleScreen for i in range(verticalPrecisionParameter+1)]
-
-
-def printGraph():
-    for i in range(len(a)):
-        for j in range(len(a[i])):
-            print(a[i][j], end="")
-        print("")
-
+graphData = [[' ']*degreeSpanSingleScreen for i in range(graphPadding+1)] # empty graph declaration
 
 while True:
     for i in range(degreeSpanSingleScreen):
         sinVal = int(math.sin(math.radians(degreeFreeFlow))*10)
         if sinVal > 10:
-            sinVal = 10
-        for j in range(verticalPrecisionParameter+1):
-            a[j][graphYPosIndex] = " "
-        a[(verticalPrecisionParameter//2)-(sinVal)
-          ][graphYPosIndex] = character[characterSelection]
+            sinVal = 10 # capping the sin value for additional smooothness
+        for j in range(graphPadding+1):
+            graphData[j][graphYPosIndex] = " "
+        graphData[(graphPadding//2)-(sinVal)
+          ][graphYPosIndex] = character[characterSelection]# interpretting sin value for degree into a 2d array
         degreeFreeFlow += steepness
         graphYPosIndex += 1
         if graphYPosIndex >= degreeSpanSingleScreen-1:
-            graphYPosIndex = 0
+            graphYPosIndex = 0 # out of range error prevention
             if not switch:
                 characterSelection = not characterSelection
         if not switch:
             break
-    printGraph()
-    time.sleep(0.1)
+    printGraph() # printing the updated graph
     os.system("cls")
